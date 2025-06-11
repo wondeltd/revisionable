094827f27689a920161b31a0eb3e4ab7e18fd8fa
Keith Hill <khill@wonde.com>
2025-06-11 11:56:05 +0100

    DS-2114 - Support revisions in MDP
    This requires connection switching support, to store revisions in school databases.

--- src/Venturecraft/Revisionable/RevisionableTrait.php
+++ src/Venturecraft/Revisionable/RevisionableTrait.php
@@ -213,7 +213,7 @@ trait RevisionableTrait
                     }
                 }
                 $revision = Revisionable::newModel();
-                \DB::table($revision->getTable())->insert($revisions);
+                \DB::connection($revision->getConnectionName())->table($revision->getTable())->insert($revisions);
                 \Event::dispatch('revisionable.saved', array('model' => $this, 'revisions' => $revisions));
             }
         }
@@ -248,7 +248,7 @@ trait RevisionableTrait
             $revisions = array_merge($revisions[0], $this->getAdditionalFields());
 
             $revision = Revisionable::newModel();
-            \DB::table($revision->getTable())->insert($revisions);
+            \DB::connection($revision->getConnectionName())->table($revision->getTable())->insert($revisions);
             \Event::dispatch('revisionable.created', array('model' => $this, 'revisions' => $revisions));
         }
 
@@ -278,7 +278,7 @@ trait RevisionableTrait
             $revisions = array_merge($revisions[0], $this->getAdditionalFields());
 
             $revision = Revisionable::newModel();
-            \DB::table($revision->getTable())->insert($revisions);
+            \DB::connection($revision->getConnectionName())->table($revision->getTable())->insert($revisions);
             \Event::dispatch('revisionable.deleted', array('model' => $this, 'revisions' => $revisions));
         }
     }
@@ -309,7 +309,7 @@ trait RevisionableTrait
             );
 
             $revision = Revisionable::newModel();
-            \DB::table($revision->getTable())->insert($revisions);
+            \DB::connection($revision->getConnectionName())->table($revision->getTable())->insert($revisions);
             \Event::dispatch('revisionable.deleted', array('model' => $this, 'revisions' => $revisions));
         }
     }
